556ab5d5c158af953aacf12f91fcd0cdda7e01fe
Jaideep Kumar <jaideepkumarsingh14@gmail.com>
2022-10-09 15:37:01 +0530

    Multiply.java added

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Multiply{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println("Multiplication is : "a*b);
+	}
+}

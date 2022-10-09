5cb3af898dfe0ca4114b3515c4da842a88ea3fd6
Jaideep Kumar <jaideepkumarsingh14@gmail.com>
2022-10-09 15:11:09 +0530

    Initial commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Sum{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println(a+b);
+	}
+}

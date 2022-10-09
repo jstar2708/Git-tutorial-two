b84456e99df762520c2d9b76639e33dc633bbc3a
Jaideep Kumar <jaideepkumarsingh14@gmail.com>
2022-10-09 15:16:54 +0530

    adding Diff.java

---
+++ Diff.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Diff{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println("Diff is : "a-b);
+	}
+}
